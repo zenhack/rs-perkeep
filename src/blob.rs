c07aa1a2e99ea1a75b3a7748fd488d447aa5f851
Ian Denhardt <ian@zenhack.net>
2020-01-09 01:31:37 -0500

    Initial commit

---
+++ src/blob.rs
@@ -0,0 +1,13 @@
+use serde::{Serialize, Deserialize};
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Ref(String);
+
+impl Ref {
+    pub fn from_string(s: String) -> Self {
+        Ref(s)
+    }
+
+    pub fn from_str(s: &str) -> Self {
+        Ref::from_string(s.to_string())
+    }
+}
