91c13e297fe44b34f43b8e8bfb6190c6479f8c6f
Ian Denhardt <ian@zenhack.net>
2020-01-09 13:23:11 -0500

    Factor tests out into a table.

--- src/lib.rs
+++ src/lib.rs
@@ -16,44 +16,52 @@ mod tests {
 
     #[test]
     fn the_test() {
+        let cases = vec![
+            (
+                Common::new(Any::StaticSet(StaticSet{
+                    members: vec![],
+                    merge_sets: vec![],
+                })),
+                r#"{"camliVersion":1,"camliType":"static-set","members":[],"mergeSets":[]}"#,
+            ),
 
-        expect_result(
-            Common::new(Any::StaticSet(StaticSet{
-                members: vec![],
-                merge_sets: vec![],
-            })),
-            r#"{"camliVersion":1,"camliType":"static-set","members":[],"mergeSets":[]}"#,
-        );
-        expect_result(
-            Common::new(Any::Bytes(Bytes::new(vec![
-                Part {
-                    size: 0,
-                    loc: Some(PartLoc::BlobRef(blob::Ref::from_str("Hello, World!"))),
-                    offset: Some(1),
-                },
-                Part {
-                    size: 12,
-                    loc: None,
-                    offset: None,
-                },
-            ]))),
-            r#"{"camliVersion":1,"camliType":"bytes","parts":[{"size":0,"blobRef":"Hello, World!","offset":1},{"size":12}]}"#,
-        );
-        expect_result(
-            Common::new(Any::Symlink(file::Common{
-                shared: file::Shared::from_name(file::Name::FileName("thelink".to_string())),
-                specific: file::Symlink::SymlinkTarget("thefile".to_string()),
-            })),
-            r#"{"camliVersion":1,"camliType":"symlink","fileName":"thelink","symlinkTarget":"thefile"}"#,
-        );
-        expect_result(
-            Common::new(Any::Symlink(file::Common{
-                shared: file::Shared::from_name(file::Name::FileName("thelink".to_string())),
-                specific: file::Symlink::SymlinkTargetBytes(
-                    vec![file::MixedEncodingPath::Utf8("thefile".to_string())],
-                ),
-            })),
-            r#"{"camliVersion":1,"camliType":"symlink","fileName":"thelink","symlinkTargetBytes":["thefile"]}"#,
-        );
+            (
+                Common::new(Any::Bytes(Bytes::new(vec![
+                    Part {
+                        size: 0,
+                        loc: Some(PartLoc::BlobRef(blob::Ref::from_str("Hello, World!"))),
+                        offset: Some(1),
+                    },
+                    Part {
+                        size: 12,
+                        loc: None,
+                        offset: None,
+                    },
+                ]))),
+                r#"{"camliVersion":1,"camliType":"bytes","parts":[{"size":0,"blobRef":"Hello, World!","offset":1},{"size":12}]}"#,
+            ),
+
+            (
+                Common::new(Any::Symlink(file::Common{
+                    shared: file::Shared::from_name(file::Name::FileName("thelink".to_string())),
+                    specific: file::Symlink::SymlinkTarget("thefile".to_string()),
+                })),
+                r#"{"camliVersion":1,"camliType":"symlink","fileName":"thelink","symlinkTarget":"thefile"}"#,
+            ),
+
+            (
+                Common::new(Any::Symlink(file::Common{
+                    shared: file::Shared::from_name(file::Name::FileName("thelink".to_string())),
+                    specific: file::Symlink::SymlinkTargetBytes(
+                        vec![file::MixedEncodingPath::Utf8("thefile".to_string())],
+                    ),
+                })),
+                r#"{"camliVersion":1,"camliType":"symlink","fileName":"thelink","symlinkTargetBytes":["thefile"]}"#,
+            ),
+        ];
+
+        for (data, expect) in cases.into_iter() {
+            expect_result(data, expect);
+        }
     }
 }
